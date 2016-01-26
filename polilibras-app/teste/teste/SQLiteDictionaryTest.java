e2f6995e18863daa0bdaccb1971b37a6742da0ed
PauloHenrique <PauloHenrique@PauloNobre>
2016-01-26 14:22:45 -0300

    Teste

--- polilibras-app/teste/teste/SQLiteDictionaryTest.java
+++ polilibras-app/teste/teste/SQLiteDictionaryTest.java
@@ -6,7 +6,12 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+
 
 import br.usp.libras.data.SignDao;
 import br.usp.libras.data.SignDaoFactory;
@@ -16,19 +21,34 @@ import junit.framework.TestCase;
 
 public class SQLiteDictionaryTest extends TestCase {
 	
+	List<Sign> retorno;
+	Sign sign;
+	
+	@Mock
 	SQLiteDictionary dic = new SQLiteDictionary();
 	
-
+	@Before
+	public void init(){
+		MockitoAnnotations.initMocks(this);
+		sign = new Sign();
+		sign.setName("futebol");
+		retorno.add(sign);
+	}
+	
 	@Test
 	public void testNearSigns() {
+		
+		when(dic.nearSigns("Gosto")).thenReturn(retorno);
+		
 		List<Sign> palavra = dic.nearSigns("Gosto");
 		assertNotNull(palavra);
 	}
 
 	@Test
 	public void testSignByName() {
+		when(dic.signByName("futebol")).thenReturn(sign);
 		Sign palavra = dic.signByName("futebol");
-		assertNotNull(palavra);
+		assertEquals("futebol", palavra.getName());
 	}
 
 	@Test
